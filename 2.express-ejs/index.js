9e5b51b757f116834dbd6861e021bc1a5a22f09e
fajrCode <fajri42vandi@gmail.com>
2023-11-20 13:04:36 +0700

    add fetures 1

--- 2.express-ejs/index.js
+++ 2.express-ejs/index.js
@@ -1,12 +1,36 @@
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
+//setting the path views to be able access from near directory (not from index.js directory) but it's optional when we want to edit default path views
+//from /expressJS/views to /expressJS/2.express-ejs/views
+app.set("views", path.join(__dirname, "/views"));
 
-app.listen(5000, ()=>{
-    console.log('listening on host http://localhost:5000');
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/test", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render("test", { random: num });
+});
+
+app.get("/t/:tag", (req, res) => {
+  const { tag } = req.params;
+  res.render("tags", { tag });
+});
+
+app.get("/cats", (req, res) => {
+  const cats = ["Citty", "Willy", "Embul"];
+  res.render("cats", { cats });
+});
+
+app.get("*", (req, res) => {
+  res.render("<h1>Error 404: Page not Found</h1>");
+});
+
+app.listen(5000, () => {
+  console.log("listening on host http://localhost:5000");
+});
