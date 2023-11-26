b585cd567249f64cef7e0759c6aa739eb828242b
fajrCode <fajri42vandi@gmail.com>
2023-11-26 22:12:14 +0700

    build own custom middleware

--- 7.middleware-express/index.js
+++ 7.middleware-express/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-const morgan = require('morgan'); //package to show log from activity req client
+const morgan = require("morgan"); //package to show log from activity req client
 
-app.use(morgan('tiny')); // middleware is use first before execute code above
+app.use(morgan("tiny")); // middleware is use first before execute code above
+//test middleware custom
+app.use((req,res, next) => {
+  console.log("hello middleware");
+  next();
+  console.log("hello middleware after next");
+
+});
+
+app.use((req,res, next) => {
+    console.log("hello middleware 2");
+    next();
+  });
 
 app.get("/", (req, res) => {
   res.send("Response Success");
