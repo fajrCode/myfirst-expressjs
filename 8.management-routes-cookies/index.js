804cb552d7d8ecd6e378af487a6d9258e03a6ed3
fajrCode <fajri42vandi@gmail.com>
2023-11-27 19:00:40 +0700

    try signed cookie

--- 8.management-routes-cookies/index.js
+++ 8.management-routes-cookies/index.js
@@ -9,12 +9,24 @@ const port = 5000;
 //define middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('secret-key')); //give parameter to make secret cookie or signing cookie
 
 //define routes
 app.use("/admin", require("./routes/admin"));
 app.use("/theater", require("./routes/theater"));
 app.use("/movies", require("./routes/movies"));
+
+//test define signed cookie
+app.get("/signingcookie", (res, req) => {
+  res.cookie("paket", "runsel", { signed: true });
+  res.send("signing cookie");
+});
+
+//test verify signed cookie
+app.get("/verifycookie", (res, req) => {
+  const cookies = res.signedCookies;
+  res.send(cookies);
+});
 
 //start server
 app.listen(port, () => {
