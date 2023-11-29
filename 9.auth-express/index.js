86a34ba69acda2417e148c249d719d62124d0d2e
fajrCode <fajri42vandi@gmail.com>
2023-11-29 09:41:38 +0700

    add authentication basic

--- 9.auth-express/index.js
+++ 9.auth-express/index.js
@@ -1,18 +1,61 @@
-const bcrypt = require("bcrypt");
+const express = require("express");
+const app = express();
+const port = 5000;
+const db = require("./connection");
+const bodyParser = require("body-parser");
+const { hashPassword, check } = require("./auth");
 
-const hashPassword = async (pw) => {
-  const hash = await bcrypt.hash(pw, 12); //12 is salt
-  console.log(hash);
-};
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const login = async (pw, hashedpw) => {
-  const result = await bcrypt.compare(pw, hashedpw);
-  if (result) {
-    console.log("login successfully");
-  } else {
-    console.log("password is incorrect");
-  }
-};
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
-hashPassword("ayam");
-login("ayam", '$2b$12$Z/oa5DoJgChDYsMZ/aGea.ZBtiPsQDRPFEPVuqqJ8dWGhlC2a1BiK');
+app.get("/", (req, res) => {
+  res.send("Response Success");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, pw } = req.body;
+
+  const pwh = await hashPassword(pw);
+
+  const sql = `INSERT INTO users (username,pw) VALUES ('${username}','${pwh}')`;
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  const { username, pw } = req.body;
+  const sql = `SELECT pw FROM users WHERE username='${username}'`;
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    if (!result.RowDataPacket) {
+      console.log("Tidak ada data yang ditemukan");
+      res.send("Data not Found");
+    } else {
+      res.send(result[0].pw);
+      console.log(result[0].pw);
+      check(pw, result[0].pw);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
