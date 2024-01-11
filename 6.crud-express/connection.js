afb1f9425b483399890ddbed1971543ce8426b77
fajrCode <fajri42vandi@gmail.com>
2024-01-11 12:05:55 +0700

    move repo

--- 6.crud-express/connection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql");
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "dbmhs",
-});
-
-module.exports = db;
--- 9.auth-express/connection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql");
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "db_auth",
-});
-
-module.exports = db;
